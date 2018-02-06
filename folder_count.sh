e508d03dddd298a964a37656b2815a1b86633115
Sharma <ds.kd0232@gmail.com>
2018-02-06 08:36:35 +0000

    Sharma's comment

--- folder_count.sh
+++ folder_count.sh
@@ -14,3 +14,4 @@ echo the no of folder inside $folder is : `ls  -lR $folder | grep ^d | wc -l`
 # this is the program to count the no of folders inside a folder. 
 
 #Testing deepak's account access
+# Hey Deepak I'm wokring in this project too
