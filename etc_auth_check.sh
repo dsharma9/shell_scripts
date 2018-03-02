f814cf7f2f5856a0d6ca79cf07b07d8ccb6c3031
Deepak <ds.kd0232@gmail.com>
2018-03-02 23:31:51 +0000

    Adding etc auth file to the Shell_scripting repo

---
+++ etc_auth_check.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "###########################This script will check the Password Authentication mode##################################"
+
+sudo su -
+
+auth=`grep ^PasswordAuthentication /etc/ssh/sshd_config | cut -d" " -f2`
+
+
+
+echo "The Pasword Auth mode is set to $auth"
+
+
+exit
