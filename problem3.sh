17d056e1abdf949247e4085a40f2d7e97332a6f1
Deepak <ds.kd0232@gmail.com>
2018-03-03 05:57:59 +0000

    hia

--- problem3.sh
+++ problem3.sh
@@ -8,6 +8,6 @@ read folder
 count=`find $folder  -type d -empty  2>/dev/null  2>/dev/null | wc -l`
 
 
-echo 'The no of empty folder inside $folder is $count'
+echo "The no of empty folder inside $folder is $count"
 
 
