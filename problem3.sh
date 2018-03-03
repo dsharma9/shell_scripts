cbd33d256734c7e6674c60e40e6c7b386e0a993d
Deepak <ds.kd0232@gmail.com>
2018-03-03 06:01:26 +0000

    adding problem3.sh

--- problem3.sh
+++ problem3.sh
@@ -2,12 +2,11 @@
 
 echo "This script the no of empty folder"
 
-read folder
 
 
-count=`find $folder  -type d -empty  2>/dev/null  2>/dev/null | wc -l`
+count=`find /etc  -type d -empty  2>/dev/null  2>/dev/null | wc -l`
 
 
-echo "The no of empty folder inside $folder is $count"
+echo "The no of empty folder inside /etc is $count"
 
 
